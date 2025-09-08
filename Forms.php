6dfc6035b5563d249c4ec3aa0be9dbdf77eef0f8
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-08 10:46:28 +0100

    update

---
+++ Forms.php
@@ -0,0 +1,16 @@
+<?php
+echo "!DOCTYPE html";
+echo "<html>";
+echo "<head>";
+
+echo "</head>";
+
+echo "<body>";
+    echo "<form method='post' action=''>";
+
+    echo "</form>";
+echo "</body>";
+echo "</html>";
+
+
+?>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+echo "!DOCTYPE html";
+
+echo "<html>";
+
+echo "<head>";
+echo"<title>OccSpec 2</title>";
+
+echo "<img = src='images/diagram-css.jpg', alt='diagram'>";
+
+echo"<link rel='stylesheet' href='css/styles.css'>";
+
+echo "</head>";
+
+echo "<body>";
+
+    echo "<h1>CSS-Cascading Style Sheet </h1>";
+    echo "<P id='today'>Is a language that is mainly used in html website</P>";
+    echo "<P>CSS is designed to enable the separation of content and presentation, 
+    including layout, colors, and fonts.</P>";
+
+echo "<hr>";
+
+echo "<table>";
+
+    echo "<tr>";
+
+    echo "
+    <tr><td> </td><td> </td><td> </td></tr>
+    <tr><td> </td><td> </td><td> </td></tr>
+    <tr><td> </td><td> </td><td> </td></tr>
+    <tr><td> </td><td> </td><td> </td></tr>";
+
+    echo"</tr>";
+echo"</table>";
+
+echo "<ul>";
+
+
+echo "</ul>"
+echo"</body>";
+
+echo "</html>";
+
+
+
