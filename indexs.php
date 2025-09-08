f0a4f30e135f4950ec83c4d09412809136667b0d
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-08 09:08:17 +0100

    update

--- indexs.php
+++ indexs.php
@@ -4,7 +4,7 @@ echo "!DOCTYPE html";
 
 echo "<html>";
 echo "<head>";
-echo"<title>OccSpec 2</title>"
+echo"<title>OccSpec 2</title>";
 echo "<link rel=;'stylesheet' href='styles.css'>";
 echo "</head>";
 
