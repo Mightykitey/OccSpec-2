84926d95644f3f14bdc7226feedef07735104b32
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-08 09:04:55 +0100

    Update

---
+++ indexs.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "!DOCTYPE html";
+
+echo "<html>";
+echo "<head>";
+echo"<title>OccSpec 2</title>"
+echo "<link rel=;'stylesheet' href='styles.css'>";
+echo "</head>";
+
+echo "<body>";
+    echo "<h1>Css</h1>";
+
+
+
+?>
