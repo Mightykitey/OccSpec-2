cee9e362d4825834a205a01614e7fb87ee5151b9
Kite.jarvis <Mightykitey1@gmail.com>
2025-09-08 11:31:32 +0100

    update

--- forms.php
+++ forms.php
@@ -1,14 +1,14 @@
 <?php
-echo "!DOCTYPE html";
+echo "<!DOCTYPE html>";
 echo "<html>";
 echo "<head>";
-echo "<a href='index.php'> go back to main page</a> ";
+echo"<title>The Forms</title>";
 echo "</head>";
 
 echo "<body>";
     echo "<form method='post' action=''>";
-        echo"<label for='num'> No. of tickect </label>";
-        // this a label infront of the box's
+        echo"<label for='num'>No. of ticket </label>";
+        // this a label in front of the box's
         echo "<input type='text' name='num' id='num' placeholder='number of tickets required'>";
         // most thing start with an input. declare a name(num). placeholder text in the box's
         echo "<br>";
@@ -19,12 +19,9 @@ echo "<body>";
         echo"<label for='lgn'> Login in </label>";
         echo "<input  type='submit' name='submit' id='lgn' value='login'>";
         //
-
-
-
-    echo "</form>";
-
-echo"<link rel='stylesheet' href='css/styles.css'>";
+        echo "</form>";
+    echo "<a href='index.php'> go back to main page</a> ";
+    echo"<link rel='stylesheet' href='css/styles.css'>";
 echo "</body>";
 echo "</html>";
 
